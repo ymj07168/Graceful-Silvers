462511023b125ec11b678853ca53e8b0cf04ed14
이효원 <ymj07168@gmail.com>
2023-04-01 14:30:06 +0900

    코드업 파이썬 기초 100제 풀이 ~44

---
+++ hyowon/codeup.py
@@ -0,0 +1,16 @@
+# 코드업
+# Python 기초 100제
+
+# 6019
+# y, m, d = input().split('.')
+# print(d, m, y, sep='-')
+
+# 6020
+# birth, info = input().split('-')
+# print(birth+''+info)
+
+#6033
+# n = ord(input())
+# print(chr(n+1))
+
+#6044
