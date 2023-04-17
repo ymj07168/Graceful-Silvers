8446f3ca64b585cfeee912529e89939cacfe62a0
minjikim <79203421+Lightieey@users.noreply.github.com>
2023-04-17 14:35:51 +0900

    Update Ex4_2.java

--- minji/week4/Ex4_2.java
+++ minji/week4/Ex4_2.java
@@ -6,13 +6,6 @@ import java.io.InputStreamReader;
 
 public class Ex4_2 {
 
-	public static boolean check(int h, int m, int s) {
-		// h -> 1의 자리가 3, m -> 30이거나 1의 자리가 3, s -> 30이거나 1의 자리가 3
-		if (h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3)
-			return true;
-		return false;
-	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
@@ -21,11 +14,8 @@ public class Ex4_2 {
 		for (int i = 0; i <= N; i++) {
 			for (int j = 0; j < 60; j++) {
 				for (int k = 0; k < 60; k++) {
-					// String time = Integer.toString(N).concat(Integer.toString(j)).concat(Integer.toString(k));
-					// System.out.println(time);
-					// if (time.contains("3")) count++;
-
-					if (check(i, j, k)) count++;
+					String time = Integer.toString(i).concat(Integer.toString(j)).concat(Integer.toString(k));
+					if (time.contains("3")) count++;
 				}
 			}
 		}
