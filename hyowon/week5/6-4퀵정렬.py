b71b8fce4c87c7168a4832dc43b14c85283de779
이효원 <ymj07168@gmail.com>
2023-05-06 18:05:44 +0900

    [Ch06 정렬] 6-4 퀵정렬

--- hyowon/week5/6-4퀵정렬.py
+++ hyowon/week5/6-4퀵정렬.py
@@ -1,3 +1,4 @@
+# 퀵 정렬
 array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 
 def quick_sort(array, start, end):
@@ -7,12 +8,16 @@ def quick_sort(array, start, end):
     left = start + 1
     right = end
     while left <= right:
+        # 피벗보다 큰 데이터를 찾을 때까지 반복
         while left <= end and array[left] <= array[pivot]:
             left += 1
+        # 피벗보다 작은 데이터를 찾을 때까지 반복
         while right > start and array[right] >= array[pivot]:
             right -= 1
+        # 엇갈린 경우 => 작은 데이터와 피벗을 교체
         if left > right:
             array[right], array[pivot] = array[pivot], array[right]
+        # 엇갈리지 않은 경우 => 작은 데이터와 큰 데이터를 교체
         else:
             array[left], array[right] = array[right], array[left]
 
