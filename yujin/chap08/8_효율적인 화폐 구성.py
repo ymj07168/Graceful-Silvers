1808be2dae0b33bf97ec52dcf96a04a259842681
Yujin Jeong <yujin45@sookmyung.ac.kr>
2023-05-22 12:32:12 +0900

    다이나믹 프로그래밍

--- yujin/chap08/8_효율적인 화폐 구성.py
+++ yujin/chap08/8_효율적인 화폐 구성.py
@@ -4,14 +4,14 @@ array = []
 for i in range(n):
     array.append(int(input()))
 
-d = [99999] * (m+1)
+d = [10001] * (m+1)
 d[0] = 0
 
 for i in range(n):
     for j in range(array[i], m+1):
         d[j] = min(d[j], d[j-array[i]] + 1)
 
-if d[m] == 99999:
+if d[m] == 10001:
     print(-1)
 else:
     print(d[m])
