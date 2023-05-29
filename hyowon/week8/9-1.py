4779a5cc1c2d70a88c3e5b96742b5e1c408cc884
이효원 <ymj07168@gmail.com>
2023-05-29 13:21:45 +0900

    [Ch09 다익스트라알고리즘] 9-1

---
+++ hyowon/week8/9-1.py
@@ -0,0 +1,51 @@
+import sys
+input = sys.stdin.readline()
+INF = int(1e9)
+
+# 노드의 개수, 간선의 개수 입력받기
+n, m = map(int, input().split())
+# 시작 노드 번호를 입력받기
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n+1)]
+# 방문한 적이 있는지 체크하는 목적의 리스트 만들기
+visited = [False] * (n+1)
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n+1)
+
+# 모든 간선 정보를 입력받기
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # a번 노드에서 b번 노드로 가는 비율이 c라는 의미
+    graph[a].append((b,c))
+
+def get_smallest_nod():
+    min_value = INF
+    index = 0
+    for i in range(1, n+1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+        return index
+
+def dijkstra(start):
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    for i in range(n-1):
+        now = get_smallest_nod()
+        visited[now] = True
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+dijkstra(start)
+
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n+1):
+    if distance[i] == INF:
+        print('INFINITY')
+    else:
+        print(distance[i])
